56919f4fc0d989593837a43258b8c5405dc9b0ca
Tyler Choi <tyler.won.choi@gmail.com>
2020-08-31 11:43:53 -0700

    App Panel creation in AppTemplate.java/cms folder needs attention

--- src/gov/nasa/cms/CMSStereo.java
+++ src/gov/nasa/cms/CMSStereo.java
@@ -10,7 +10,8 @@ import gov.nasa.worldwind.Configuration;
 import gov.nasa.worldwind.avlist.AVKey;
 import gov.nasa.worldwind.*;
 import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
-import gov.nasa.worldwindx.examples.util.LayerManagerLayer;
+import gov.nasa.worldwind.geom.Angle;
+import gov.nasa.worldwind.geom.Position;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -24,13 +25,9 @@ public class CMSStereo extends JCheckBoxMenuItem
 {
 
     private WorldWindow wwd;
-//    private ShutdownWindowAction shutdownAction;
-//    private CreateWindowAction createWindowAction;
     private boolean isItemEnabled;
-    //public static final String APP_NAME = "Celestial Mapping System";
-    //private StereoOptionSceneController stereoController;
     private WorldWindowGLCanvas wwc;
-    private WorldWindowGLCanvas originalCanvas;
+    //private WorldWindowGLCanvas originalCanvas;
     
     public WorldWindow getWwd()
     {
@@ -44,11 +41,8 @@ public class CMSStereo extends JCheckBoxMenuItem
     
     public CMSStereo(AppFrame cms, WorldWindow window)
     {   
-        //JCheckBoxMenuItem item = new JCheckBoxMenuItem("Stereo");
         super("Stereo");
 
-       //window = cms.wwjPanel.getWwd();
-       originalCanvas = (WorldWindowGLCanvas) cms.wwjPanel.getWwd();
         
         this.addActionListener(new ActionListener()
         {
@@ -57,8 +51,6 @@ public class CMSStereo extends JCheckBoxMenuItem
             {             
                 isItemEnabled = ((JCheckBoxMenuItem) event.getSource()).getState();
                 
-                
-
                 if (isItemEnabled) {
                     
                     
@@ -67,8 +59,8 @@ public class CMSStereo extends JCheckBoxMenuItem
                         //WorldWind.shutDown();
                         cms.wwjPanel.getWwd().shutdown();
                         //window.shutdown();
-                        cms.getContentPane().remove((Component) cms.wwjPanel);
-                        //cms.getContentPane().remove((Component) cms.wwjPanel.getWwd());
+                        cms.getContentPane().remove((Component) cms.wwjPanel); //removing component's parent must be JPanel
+                        //cms.getContentPane().remove((Component) cms.wwjPanel.getWwd()); //does not work bc wwd parent is AppPanel, not JPanel
                         //cms.wwjPanel.wwd = null;
                     }
                     else {
@@ -85,52 +77,29 @@ public class CMSStereo extends JCheckBoxMenuItem
 
                     wwc.setPreferredSize(cms.wwjPanel.getSize());
                     wwc.setModel(window.getModel());
-                    //cms.createAppPanel(cms.wwjPanel.getSize(), true);
                     
                     
+                    //below 2 lines initialize layer panel and control layer to original cms window but disables wwd connection with place names
 //                    cms.initialize(true, false, false);
 //                    cms.getWwd().getModel().getLayers().add(new LayerManagerLayer(cms.getWwd()));
                     
                     cms.getContentPane().add(wwc, java.awt.BorderLayout.CENTER);
-                    //cms.getContentPane().add(cms.getWwjPanel(), java.awt.BorderLayout.CENTER);
-                                        
-                    //cms.getContentPane().add(wwc.getParent(), java.awt.BorderLayout.WEST);
+
                     cms.pack();
-                    //cms.setEnabled(true);
-                                  
-                    //this // ApplicationTemplate$AppPanel
-                    //this.wwd // WorldWindowGLCanvas
-                    //this.wwd.wwd // WorldWindowGLAutoDrawable
-                    //this.wwd.wwd.sceneController // StereoOptionSceneController
-                    
-                    
-                    // StereoOptionSceneController();
-                     
-                    //this.createWorldWindow().wwd.sceneController.setStereoMode(AVKey.STEREO_MODE_RED_BLUE);
-                    //this.wwd.wwd.sceneController.setStereoMode(AVKey.STEREO_MODE_RED_BLUE);
-                    
-                    //ApplicationTemplate.start("Stereo View", CelestialMapper.AppFrame.class); 
-                    /* launches a new window with stereo view enabled. 
-                     * StereoOptionSceneController.java documentation states that stereo view property must be set before calling the constructor
-                    */
-                   
                 }
                 else {
                              
-                    //WorldWindowGLCanvas wwc = new WorldWindowGLCanvas();
                     wwc.shutdown();
                     cms.getContentPane().remove((Component) wwc);
                     //cms.getContentPane().remove((Component) cms.wwjPanel);
                     wwc = null;
-
-                    //wwc = cms.wwjPanel.getwWd*();
-                    //wwc = window.
                     
                     disableStereo();
+                    
+
 //                    cms.initialize(true, false, false);
 //                    cms.getWwd().getModel().getLayers().add(new LayerManagerLayer(cms.getWwd()));
 //                    cms.importElevations();
-                    //wwc = new WorldWindowGLCanvas();
                     
                     wwc = (WorldWindowGLCanvas) cms.wwjPanel.createWorldWindow();
                     //wwc = (WorldWindowGLCanvas) cms.getWwd();
@@ -140,26 +109,16 @@ public class CMSStereo extends JCheckBoxMenuItem
                     wwc.setModel(new BasicModel());
                     
                     
-                    //cms.getWwd().getModel().getLayers().add(new LayerManagerLayer(cms.getWwd()));
-                    
-                    //wwc = (WorldWindowGLCanvas) cms.wwjPanel.createWorldWindow();
-
                     cms.getContentPane().add(wwc, java.awt.BorderLayout.CENTER);
-                    //cms.getContentPane().add((Component) cms.wwjPanel, java.awt.BorderLayout.CENTER);
-                    
-
                     cms.pack();
                     
                 }
             }
         });
-        
-       
-        
-        
-        
+      
     }
     
+    //set stereo view property in system
     private void launchStereoView()
     {
         // Set the stereo.mode property to request stereo. Request red-blue anaglyph in this case. Can also request
@@ -176,75 +135,18 @@ public class CMSStereo extends JCheckBoxMenuItem
         //ApplicationTemplate.start("Stereo View", CelestialMapper.AppFrame.class); 
     }
     
+    //disables stereo view property in system
     private void disableStereo()
     {
         System.setProperty("gov.nasa.worldwind.stereo.mode", "");
-        Configuration.setValue(AVKey.INITIAL_LATITUDE, 20);
-        Configuration.setValue(AVKey.INITIAL_LONGITUDE, 30);
-        Configuration.setValue(AVKey.INITIAL_ALTITUDE, 10e4);
-        Configuration.setValue(AVKey.INITIAL_HEADING, 500);
-        Configuration.setValue(AVKey.INITIAL_PITCH, 80);
-        //System.setProperty("java.net.useSystemProxies", "true
+//        Configuration.setValue(AVKey.INITIAL_LATITUDE, 20);
+//        Configuration.setValue(AVKey.INITIAL_LONGITUDE, 30);
+//        Configuration.setValue(AVKey.INITIAL_ALTITUDE, 10e4);
+//        Configuration.setValue(AVKey.INITIAL_HEADING, 500);
+//        Configuration.setValue(AVKey.INITIAL_PITCH, 80);
         
     }
-    
-//    private void createWindow()
-//    {
-//        WorldWindowGLCanvas wwc = new WorldWindowGLCanvas();
-//        wwc.setPreferredSize(new java.awt.Dimension(800, 600));
-//        getContentPane().add(wwc, java.awt.BorderLayout.CENTER);
-//        wwc.setModel(new BasicModel());
-//        this.wwd = wwc;
-//    }
-//    
-//    private void destroyCurrentWindow()
-//    {
-//        if (this.wwd != null)
-//        {
-//            getContentPane().remove((Component) wwd);
-//            wwd = null;
-//        }
-//    }
-//
-//    private class ShutdownWindowAction extends AbstractAction
-//    {
-//        public ShutdownWindowAction()
-//        {
-//            super("Shutdown Window");
-//        }
-//
-//        public void actionPerformed(ActionEvent e)
-//        {
-//            if (wwd != null)
-//            {
-//                wwd.shutdown();
-//                destroyCurrentWindow();
-//                this.setEnabled(false);
-//                createWindowAction.setEnabled(true);
-//            }
-//        }
-//    }
-//
-//    private class CreateWindowAction extends AbstractAction
-//    {
-//        public CreateWindowAction()
-//        {
-//            super("Create Window");
-//            this.setEnabled(false);
-//        }
-//
-//        public void actionPerformed(ActionEvent e)
-//        {
-//            createWindow();
-//            pack();
-//            this.setEnabled(false);
-//            shutdownAction.setEnabled(true);
-//        }
-//    } 
-    
-    
-    
-    
+          
 //    public static void main(String[] args)
 //    {
 //        // Set the stereo.mode property to request stereo. Request red-blue anaglyph in this case. Can also request
